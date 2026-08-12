d0e03f882ea8965305f9ff44a7e1a3231ce58161
sanket <sanketchounde.riyality@gmail.com>
2026-08-12 23:55:47 +0530

    Testing Github

--- ShelfTrack/src/pages/Index.jsx
+++ ShelfTrack/src/pages/Index.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Index = () => {
   return (
     <div>
-      <h1>hello</h1>
+      <h1>Hello From Sanket</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
