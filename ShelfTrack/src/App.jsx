0e0332fede16b4bbc7d3cf7636c1d28654d358d5
manodhairy <manodhairygalave9322@gmail.com>
2026-08-14 11:30:05 +0530

    routing done

--- ShelfTrack/src/App.jsx
+++ ShelfTrack/src/App.jsx
@@ -1,39 +1,25 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-import AppLayout from "./components/AppLayout";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import ManageBooks from "./pages/ManageBooks";
 import ManageMembers from "./pages/ManageMembers";
-import IssueDesk from "./pages/IssueDesk";
 
-import "./App.css";
+import AppLayout from "./components/AppLayout";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
-      <ToastContainer position="top-right" autoClose={3000} />
-
       <Routes>
-        {/* LOGIN */}
-
-        <Route path="/" element={<Navigate to="/login" replace />} />
-
         <Route path="/login" element={<Login />} />
 
-        {/* APPLICATION LAYOUT */}
-
-        <Route element={<AppLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-
-          <Route path="/books" element={<ManageBooks />} />
-
-          <Route path="/members" element={<ManageMembers />} />
-
-          <Route path="/issue-desk" element={<IssueDesk />} />
+        <Route element={<ProtectedRoute />}>
+          <Route element={<AppLayout />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/books" element={<ManageBooks />} />
+            <Route path="/members" element={<ManageMembers />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
