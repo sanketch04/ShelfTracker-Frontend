300ca11118da7a37bfabf37291d771f1cf3d59e3
manodhairy <manodhairygalave9322@gmail.com>
2026-08-14 11:54:57 +0530

    logout done

--- ShelfTrack/src/components/ProtectedRoute.jsx
+++ ShelfTrack/src/components/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ const ProtectedRoute = () => {
   const user = localStorage.getItem("user");
 
   if (!user) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/" replace />;
   }
 
   return <Outlet />;
